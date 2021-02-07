a4a5838b0efd98c6041b193050d0ba89269c4806
Avesh Khan <78172638+Aveshkhan@users.noreply.github.com>
2021-02-08 01:37:53 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,54 @@
+// Show Navbar
+const showNavbar = (toggleId, navId, bodyId, headerId) => {
+    const toggle = document.getElementById(toggleId),
+        nav = document.getElementById(navId),
+        bodypd = document.getElementById(bodyId),
+        headerpd = document.getElementById(headerId)
+
+    //validating all variable exist
+    if (toggle && nav && bodypd && headerpd) {
+        toggle.addEventListener('click', ()=>{
+
+            //show navbar
+            nav.classList.toggle('show')
+            //change icon
+            toggle.classList.toggle('bx-x')
+            //add padding to body
+            bodypd.classList.toggle('body-pd')
+            //add padding to header
+            headerpd.classList.toggle('body-pd')
+
+        })
+    }
+}
+
+showNavbar('header-toggle', 'nav-bar','body-pd', 'header')
+
+// Link Active
+const linkColor = document.querySelectorAll('.nav__link')
+
+function colorLink(){
+    if(linkColor){
+        linkColor.forEach(l => l.classList.remove('active'))
+        this.classList.add('active')
+    }
+}
+
+linkColor.forEach(l => l.addEventListener('click', colorLink))
+
+// Clock
+var interval = setInterval(timestamphome, 1000);
+
+
+
+
+ function timestamphome(){
+ var date;
+date = new Date();
+
+ 
+ var time = document.getElementById('timediv'); 
+ time.innerHTML = date.toLocaleTimeString();
+
+
+  }
